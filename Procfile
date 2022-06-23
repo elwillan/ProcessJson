a753ee53d964fbf381079ad5525f857baaaf74a3
elwillan <williamgarnica@ingenieros.com>
2022-06-22 19:03:02 -0500

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/ProcessJson-0.0.1-SNAPSHOT.jar
+web: java -jar build/libs/ProcessJson-0.0.1-SNAPSHOT.jar
--- src/main/java/ProcessService.java
+++ src/main/java/ProcessService.java
@@ -40,7 +40,7 @@ public class ProcessService {
         return result;
     }
 
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void maain(String[] args) throws JsonProcessingException {
         ProcessService test = new ProcessService();
         String jsonString = "{\n" +
                 "\"text\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad\\nminim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit\\nin voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\\ndeserunt mollit anim id est laborum.\",\n" +
